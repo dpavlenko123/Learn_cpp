525539cce7664a3070c38851c8ee6199c5c42e33
dpavlenko123 <felichia_dana@mail.ru>
2026-04-23 23:23:20 +0300

    Delete base/lesson4/hw/exceptions.hpp

--- base/lesson4/hw/exceptions.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdexcept>
-#include <iostream>
-#include <string>
-class СompilerError : public std::runtime_error {  
-public:
-    СompilerError(const std::string& msg): std::runtime_error(msg) {}
-};
-
